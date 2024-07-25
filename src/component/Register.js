f1cc2cd29692060605cec2a70fa1361e4c3addc2
unknown <poulagani.pp@gmail>
2024-07-25 19:07:44 +0530

    post call updated..

--- src/component/Register.js
+++ src/component/Register.js
@@ -5,11 +5,10 @@ import './Register.css';
 
 const FormComponent = () => {
   const [formData, setFormData] = useState({
-    emoplyeeId: '',
+    employeeId: '',
     destinationUrl: '',
     description: '',
     linkname:''
-
   });
 
   const [errors, setErrors] = useState({});
@@ -21,15 +20,15 @@ const FormComponent = () => {
   };
 
   const validateForm = () => {
-    const { emoplyeeId, description, linkname } = formData;
+    const { employeeId, description, linkname } = formData;
     const errors = {};
 
-    if (!emoplyeeId) {
-      errors.emoplyeeId = 'Employee ID is required';
-    } else if (!/^\d+$/.test(emoplyeeId)) {
-      errors.emoplyeeId = 'Employee ID must be a number';
-    } else if (emoplyeeId.length < 5 && emoplyeeId.length >= 10) {
-      errors.emoplyeeId = 'Employee ID must be greater than 5 and less than 10 digits';
+    if (!employeeId) {
+      errors.employeeId = 'Employee ID is required';
+    } else if (!/^\d+$/.test(employeeId)) {
+      errors.employeeId = 'Employee ID must be a number';
+    } else if (employeeId.length < 5 && employeeId.length >= 10) {
+      errors.employeeId = 'Employee ID must be greater than 5 and less than 10 digits';
     }
     if(description.length > 40){
         errors.description = 'Description cannot be more than 40 characters'
@@ -49,19 +48,37 @@ const FormComponent = () => {
       return;
     }
     
-   axios.get('https://eighth-duality-422111-h3.df.r.appspot.com/Go/hello')
-  .then(response => {
-    // Handle success
-    console.log("data fetched successfully........")
-    console.log(response.data);
+  //  axios.get('https://eighth-duality-422111-h3.df.r.appspot.com/Go/hello')
+  // .then(response => {
+  //   // Handle success
+  //   console.log("data fetched successfully........")
+  //   console.log(response.data);
+  // })
+  // .catch(error => {
+  //   console.error('Error fetching data:', error);
+  // });
+
+  axios.post('https://eighth-duality-422111-h3.df.r.appspot.com/go/create', {
+    employeeId : formData.employeeId,
+    destinationUrl: formData.destinationUrl,
+    shortLink : formData.linkname,
+    description: formData.description
   })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
+    .then(response => {
+      // Handle success
+      console.log("Data posted successfully........");
+      console.log(response.data);
+    })
+    .catch(error => {
+      // Handle error
+      console.error('Error posting data:', error);
+    });
+
+
 
     console.log(formData);
     setFormData({
-        emoplyeeId: '',
+        employeeId: '',
         destinationUrl: '',
         description: '',
         linkname:''
@@ -72,16 +89,16 @@ const FormComponent = () => {
     <div className="form-container">
         <h1 id ="header">Create New Go Link</h1>
       <form onSubmit={handleSubmit} className="form">
-        <label htmlFor="emoplyeeId">Employee Id:</label>
+        <label htmlFor="employeeId">Employee Id:</label>
         <input
           type="text"
-          id="emoplyeeId"
-          name="emoplyeeId"
-          value={formData.emoplyeeId}
+          id="employeeId"
+          name="employeeId"
+          value={formData.employeeId}
           onChange={handleChange}
           required
         />
-    {errors.emoplyeeId && <span className="error">{errors.emoplyeeId}</span>}
+    {errors.employeeId && <span className="error">{errors.employeeId}</span>}
   
       <label htmlFor="destinationUrl">Destination URL:</label>
         <input
@@ -92,20 +109,18 @@ const FormComponent = () => {
           onChange={handleChange}
           required
         />
-          <div className="linkname-group">
-            <div className="linkname-label">
-              <button>GO/</button>
-            </div>
-            <div className="linkname-input">
-              <input
-                type="text"
-                id="linkname"group
-                name="linkname"
-                value={formData.linkname}
-                onChange={handleChange}
-                required
-              />
-          </div>
+          <div className="form-group">
+          <label htmlFor="linkname">Link Name</label>
+          <input
+            type="text"
+            id="linkname"
+            name="linkname"
+            value={formData.linkname}
+            onChange={handleChange}
+            required
+            pattern="go/.*"
+            placeholder="go/"
+          />
           </div>
      {errors.linkname && <span className="error">{errors.linkname}</span>}
         <label htmlFor="description">Description:</label>
